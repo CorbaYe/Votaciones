bf9849274e848b6d4e979f93ff3d0d7420c36d72
Yeison Cordoba <corba.ye99@gmail.com>
2024-03-13 16:27:48 -0500

    creación de proyecto y clase cls_candidatos

---
+++ cls_candidatos.java
@@ -0,0 +1,48 @@
+public class cls_candidatos{
+    private int nurmero_int;
+    private String nombre_str;
+    private String propuesta_str;
+    private String partido_str;
+
+    public cls_candidatos(int nurmero_int, String nombre_str, String propuesta_str, String partido_str) {
+        this.nurmero_int = nurmero_int;
+        this.nombre_str = nombre_str;
+        this.propuesta_str = propuesta_str;
+        this.partido_str = partido_str;
+    }
+
+    public int getNurmero_int() {
+        return nurmero_int;
+    }
+
+    public void setNurmero_int(int nurmero_int) {
+        this.nurmero_int = nurmero_int;
+    }
+
+    public String getNombre_str() {
+        return nombre_str;
+    }
+
+    public void setNombre_str(String nombre_str) {
+        this.nombre_str = nombre_str;
+    }
+
+    public String getPropuesta_str() {
+        return propuesta_str;
+    }
+
+    public void setPropuesta_str(String propuesta_str) {
+        this.propuesta_str = propuesta_str;
+    }
+
+    public String getPartido_str() {
+        return partido_str;
+    }
+
+    public void setPartido_str(String partido_str) {
+        this.partido_str = partido_str;
+    }
+    
+
+    
+}
