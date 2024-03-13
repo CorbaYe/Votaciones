4d1ccf269261ad47aa5aded2c6d06457418ea36a
Yeison Cordoba <corba.ye99@gmail.com>
2024-03-13 16:53:37 -0500

    función menu

--- app_votos.java
+++ app_votos.java
@@ -1,5 +1,19 @@
+import java.util.LinkedList;
+
+import javax.swing.JOptionPane;
+
 public class app_votos {
+    private static LinkedList<cls_candidatos> obj_candidato = new LinkedList<>();
     public static void main(String[] args) {
-        
+        fnt_menu(true);
+    }
+
+    private static void fnt_menu(boolean sw){
+        do{
+            int option = Integer.valueOf(JOptionPane.showInputDialog(null,"<<< MENU DE OPCIONES >>>\n\n1. CANDIDATOS\n2. SALIR"));
+            if (option == 2) {
+                sw = false;
+            }
+        }while(sw);
     }
 }
